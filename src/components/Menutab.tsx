d5a6ad227d7a8996dae0b7caf2038484dba39d97
jeken527 <jeken23625@gmail.com>
2026-06-29 13:04:10 +0900

    Update Menutab.tsx

--- src/components/Menutab.tsx
+++ src/components/Menutab.tsx
@@ -12574,6 +12574,7 @@ const Menutab = (props: MenutabProps) => {
                         </div>
                     </div>
                 )}
+
                 {isSearchPopupOpen && (
                     <div style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 9999 }}>
                         <Frame63372 />
