0e26f0d3a6db557de6bc36443e57c8902a4930cd
jeken527 <jeken23625@gmail.com>
2026-07-03 22:36:03 +0900

    Update Serchbutton.tsx

--- src/components/Serchbutton.tsx
+++ src/components/Serchbutton.tsx
@@ -1,9 +1,10 @@
+import React, { useState } from "react";
 import { withStopPropagation } from "@/utils/utils";
 import "@/styles/Serchbutton.css";
 
 interface SerchbuttonProps {
     search_button_state?: string;
-    searchbutton?: string;
+    searchbutton?: string; // 원본 Props 완벽 복원
     id?: string;
     className?: string;
     mouseover?: (e: any) => void;
@@ -20,16 +21,26 @@ const Serchbutton = (props: SerchbuttonProps) => {
         click
     } = props;
 
+    // 🌟 완전 독립적인 마우스 호버 지능 탑재
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
         <div
             className={`component-55_283 ${className}`}
             id={id}
             onMouseOver={mouseover ? withStopPropagation(mouseover) : undefined}
-            onClick={click ? withStopPropagation(click) : undefined}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            onClick={(e) => {
+                // 🌟 클릭(팝업 호출) 즉시 호버를 풀어버려서 굳는 현상 방지!
+                setIsHovered(false); 
+                if (click) withStopPropagation(click)(e);
+            }}
             style={{ cursor: "pointer" }}
         >
             <div id="55_283" className="Pixso-symbol-55_283">
-                {search_button_state === "default" && (
+                {/* 🌟 1. default 상태 돋보기 (원본 디자인 복구 완료) */}
+                {!isHovered && (
                     <div id="55_282" className="stroke-wrapper-55_282">
                         <div className="Pixso-symbol-55_282">
                             <div
@@ -40,12 +51,12 @@ const Serchbutton = (props: SerchbuttonProps) => {
                         <div className="stroke-55_282"></div>
                     </div>
                 )}
-                {search_button_state === "checked" && (
+
+                {/* 🌟 2. checked 상태 돋보기 (원본 디자인 복구 완료) */}
+                {isHovered && (
                     <div id="55_281" className="stroke-wrapper-55_281">
                         <div className="Pixso-symbol-55_281">
-                            {search_button_state === "checked" && (
-                                <div className="shadow-blend-unknown-0"></div>
-                            )}
+                            <div className="shadow-blend-unknown-0"></div>
                             <div
                                 id="55_275"
                                 className="Pixso-rectangle-55_275"
@@ -58,4 +69,5 @@ const Serchbutton = (props: SerchbuttonProps) => {
         </div>
     );
 };
+
 export default Serchbutton;
