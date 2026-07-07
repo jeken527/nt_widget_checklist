e3bf638f7c081cdc64a0168b3c51643d483b39f4
jeken527 <jeken23625@gmail.com>
2026-07-07 10:21:46 +0900

    Update Menutab.tsx

--- src/components/Menutab.tsx
+++ src/components/Menutab.tsx
@@ -3988,7 +3988,7 @@ const Menutab = (props: MenutabProps) => {
                             alignItems: "center",
                             justifyContent: "center",
                             padding: "0",
-                            marginTop: "4px",
+                            margin: "0",
                             boxSizing: "border-box"
                         }}
                     >
@@ -4006,7 +4006,7 @@ const Menutab = (props: MenutabProps) => {
                                 gridTemplateColumns: "repeat(4, 1fr)",
                                 /* 🌟 세로 3줄 배치 */
                                 gridTemplateRows: "repeat(3, 1fr)",
-                                gap: "6px 3px", 
+                                gap: "2px 6px", 
                                 padding: "0",
                                 margin: "0",
                                 boxSizing: "border-box",
