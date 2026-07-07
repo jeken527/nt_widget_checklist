5aedd158c8864536ac2f43ad8cd1b8e097404822
jeken527 <jeken23625@gmail.com>
2026-07-07 14:48:31 +0900

    Update Menutab.tsx

--- src/components/Menutab.tsx
+++ src/components/Menutab.tsx
@@ -1857,7 +1857,7 @@ const Menutab = (props: MenutabProps) => {
                                 flexShrink: 0
                             }}>
                                 {/* [1] PRI (세로 중앙 정렬 장착) */}
-                                <p style={{ width: '48px', height: '20px', display: 'flex', justifyContent: 'center', alignItems: 'flex-start', margin: 0, padding: 0, flexShrink: 0, color: '#000000', fontSize: '14px' }}>
+                                <p style={{ width: '48px', height: '20px', display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start', textAlign:'center', margin: 0, padding: 0, flexShrink: 0, color: '#000000', fontSize: '14px' }}>
                                     {routine.priority}
                                 </p>
 
@@ -1893,7 +1893,7 @@ const Menutab = (props: MenutabProps) => {
                                 <div style={{ 
                                     flex: 1, 
                                     borderBottom: '1.2px solid #000000',
-                                    margin: 0, 
+                                    margin: '0 2px', 
                                     transform: 'translateY(-2px)'
                                 }}></div>
 
